de13cedbe65dce7e1edaf8be162a3f25b6ead913
Ghilman Faza <72027610+gffaza@users.noreply.github.com>
2024-08-27 14:36:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+function myFunction(p1,p2){
+    return p1 * p2 ;
+}
+
+document.getElementById("demo").innerHTML = myFunction(4,3);
---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}`);
+});
+
+
